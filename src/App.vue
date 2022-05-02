a59f6eae0c636ee53e5fa2adb9c13126bccbe400
darkraven <>
2022-05-02 15:00:26 +0900

    init commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <v-main>
+      <InputArea />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import InputArea from './components/InputArea.vue'
+
+export default {
+  name: 'App',
+
+  components: {
+    InputArea,
+  },
+
+  data: () => ({
+    //
+  }),
+}
+</script>
---
+++ src/components/InputArea.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-row>
+        <v-col cols="10" style="padding: 3;">
+            <textarea v-model="message" placeholder="edit me" style="width: 100%;" />
+        </v-col>
+        <v-col cols="2" style="padding: 3;">
+            <v-btn large color="purple" >
+                ADD
+            </v-btn>
+        </v-col>
+    </v-row>
+</template>
+
+<script>
+export default {
+  name: 'InputArea',
+
+  data: () => ({
+    message: '',
+  }),
+}
+</script>
